b05ff212fd0768298409dc22fb92655d7f9a51ca
kaustubhKVS <kvshete15@gmail.com>
2022-11-26 07:28:59 -0800

    adding minting and code formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
 use scrypto::prelude::*;
 
-// make a struct for content creator NFT
-// Make CCNFT
-// Make a CCNFT Vault
-// 
-// create hashmap for CCNFT and vault : You will need a Resourceaddress
-// create a method for adding content creator
-// create a method send_CC_money() for sending money to main vault and then to the CCNFT based vault
-// send_CC_money(&self, CCNFT , amount) : will send money to main vault then main vault will send money to CCNFT based vauly
+/*
+make a struct for content creator NFT
+Make CCNFT
+Make a CCNFT Vault
 
-// ADMIN TOKEN for Auth for creation of Creator_NFT
+create hashmap for CCNFT and vault : You will need a Resourceaddress
+create a method for adding content creator
+create a method send_CC_money() for sending money to main vault and then to the CCNFT based vault
+send_CC_money(&self, CCNFT , amount) : will send money to main vault then main vault will send money to CCNFT based vauly
+*/
+
+
+/*
+ADMIN TOKEN for Auth for creation of Creator_NFT
+*/
 
 /*
 coding rules:
@@ -47,47 +52,68 @@ struct VideoNFT {
 
 blueprint!{
     struct YtFair {
-        
-    collected_xrd: Vault,
-    shareholder_badge_resource_address: ResourceAddress,
-    internal_admin_badge: Vault,
+    
+    // VAULTS
+    collected_xrd_vault: Vault,
     cc_vaults: HashMap<NonFungibleId, Vault>,
+    
     video_vault : Vault,
+    dead_vaults: Vec<Vault>,
+    internal_admin_badge_vault: Vault,
     
-    //     hashmap<nonfungible ,  Vec<> >
-
-    // videoNFT_vault: Vault,
-    // videoNFid_array: Vec<NonFungibleId>,
-
-    // videoNFT_vault: Vault,
-    // videoNFid_array: Vec<NonFungibleId>,
-    // // video ,cc, viwer
+    // NFTs and Badges
+    shareholder_badge_resource_address: ResourceAddress,
+    video_nft:ResourceAddress,
     
-    // // video, cc :
-    //     keep track ki konsa video banaya hanging
-    //     video_ownership: HashMap<videonftID(NonFungibleId) , cc_NFT(NonFungibleId)>
-
-    // // video, viwer:
-    //     likes, views
+    // Int, string and other values
+    is_locked: bool,
+    random_card_id_counter:u64,
+    total_amount_of_shares: Decimal,
     
-    // // cc , viewer:
-    //     viwer sends money to cc:
-    //     deposit input: videonftID
-    //     output: bucket
-    //     process:
-    //         hit video_ownership and get cc_NFT
-    //         hit cc_vault and get CC ke vault ka resource address
-    //         send bucket to that resource address
+    /*
+    hashmap<nonfungible ,  Vec<> >
+    */
+
+    /*
+    videoNFT_vault: Vault,
+    videoNFid_array: Vec<NonFungibleId>,
+    */
+
+    /*
+    videoNFT_vault: Vault,
+    videoNFid_array: Vec<NonFungibleId>,
+    video ,cc, viwer
+    */
+    
+    /*
+        video, cc :
+        keep track ki konsa video banaya hanging
+        video_ownership: HashMap<videonftID(NonFungibleId) , cc_NFT(NonFungibleId)>
+    */
+
+    /*
+    video, viwer:
+    likes, views
+    */
+    
+    /*
+        cc , viewer:
+        viwer sends money to cc:
+        deposit input: videonftID
+        output: bucket
+        process:
+            hit video_ownership and get cc_NFT
+            hit cc_vault and get CC ke vault ka resource address
+            send bucket to that resource address
+    */
           
 
-    // videoNFT_vault: Vault,    
-    // videoNFT_vault: Vec<NonFungibleId>,
+    /*
+    videoNFT_vault: Vault,    
+    videoNFT_vault: Vec<NonFungibleId>,
+    */
 
-    dead_vaults: Vec<Vault>,
-    is_locked: bool,
-    total_amount_of_shares: Decimal,
-    video_nft:ResourceAddress,
-    random_card_id_counter:u64,
+    
 
     // accepted_token_resource_address: ResourceAddress,    
 }
@@ -107,7 +133,7 @@ blueprint!{
                 )
                 .initial_supply(dec!("1"));
 
-            let internal_admin_badge: Bucket = ResourceBuilder::new_fungible()
+            let internal_admin_badge_bucket: Bucket = ResourceBuilder::new_fungible()
                 .divisibility(DIVISIBILITY_NONE)
                 .metadata("name", "Internal Admin Badge")
                 .metadata("description", "An internal admin badge used for internal functionality of the PaymentSplitter.")
@@ -120,11 +146,11 @@ blueprint!{
                     "A non-fungible-token used to authenticate shareholders.",
                 )
                 .mintable(
-                    rule!(require(internal_admin_badge.resource_address())),
+                    rule!(require(internal_admin_badge_bucket.resource_address())),
                    Mutability::LOCKED,                    
                 )
                 .burnable(
-                    rule!(require(internal_admin_badge.resource_address())),
+                    rule!(require(internal_admin_badge_bucket.resource_address())),
                     Mutability::LOCKED,
                 )
                 .no_initial_supply();
@@ -145,25 +171,25 @@ blueprint!{
                     Mutability::LOCKED,
                 )
                 .no_initial_supply();
-            // random_card_id_counter=0;
-                //create vault
-        
         
 
-        //Self instantiate
+// INSTANTIATING THE RESOURCES
         let mut payment_splitter: YtFairComponent = Self {
-            collected_xrd: Vault::new(RADIX_TOKEN),
-            // accepted_token_resource_address: accepted_token_resource_address,
-            shareholder_badge_resource_address: shareholder_badge,
-            internal_admin_badge: Vault::with_bucket(internal_admin_badge),
+            
+            // Instantiate Vaults
+            collected_xrd_vault: Vault::new(RADIX_TOKEN),
+            internal_admin_badge_vault: Vault::with_bucket(internal_admin_badge_bucket),
             cc_vaults: HashMap::new(),
-            video_nft:video_nft,
             video_vault :  Vault::new(video_nft),
             dead_vaults: Vec::new(),
-            // video_nfts: Vec::new(),
+
+            // Instantiate Nfts and Badges
+            shareholder_badge_resource_address: shareholder_badge,
+            video_nft:video_nft,
+            
+            // Instantiate int , str, and other datatypes
             is_locked: false,
             total_amount_of_shares: dec!("0"),
-            
             random_card_id_counter:0,
         }
         .instantiate();
@@ -172,14 +198,23 @@ blueprint!{
             
 }
             
-            pub fn deposit(&mut self, mut payment: Bucket) -> () {
+
+
+
+
+
+// METHOD: deposit function to deposit the money
+        pub fn deposit(&mut self, mut payment: Bucket) -> () {
             // take all the money sent in the bucket in XRD
             // if the caller has sent something other than XRD, they'll get a runtime error           
-            self.collected_xrd.put(payment);
+            self.collected_xrd_vault.put(payment);
             
         }
 
-        // pub fn make_video_nft(&mut self,mut title:String,mut desc:String, mut url:String, mut ContentCreatorAddress:String) -> ()
+
+
+// METHOD: minting of video NFTs when videos are uploaded
+// pub fn make_video_nft(&mut self,mut title:String,mut desc:String, mut url:String, mut ContentCreatorAddress:String) -> ()
         pub fn make_video_nft(&mut self) -> ()
         {
             let vidz = VideoNFT {
@@ -189,7 +224,7 @@ blueprint!{
                 likes:0,
                 views:0
             };
-            // let nft_bucket:Bucket = self.internal_admin_badge(borrow_resource_manager!(self.video_nft).mint_non_fungible(
+            // let nft_bucket:Bucket = self.internal_admin_badge_vault(borrow_resource_manager!(self.video_nft).mint_non_fungible(
             //     &NonFungibleId::from_u64(self.random_card_id_counter),
             //     new_card,
             // ));
@@ -202,6 +237,8 @@ blueprint!{
 
     }
 
+
+// METHOD: SHOWING INFORMATION IN THE TOKEN
        pub fn show_token_info(address: ResourceAddress) {
            // We borrow the resource manager of the provided address
             let manager: &ResourceManager = borrow_resource_manager!(address);
@@ -224,6 +261,16 @@ blueprint!{
            let token_name = metadata.get("name").expect("Token does not have a name");
            let token_symbol = metadata.get("symbol").expect("Token does not have a symbol");
            info!("Name: {}. Symbol: {}", token_name, token_symbol);
-       } 
+       }
+       
+       
+
+
+
+
+
+
+
+
     }
 }
